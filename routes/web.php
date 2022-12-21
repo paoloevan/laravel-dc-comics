e3738e9e9a28474873bb1da0c039cb8f7ba0bed7
paoloevan <paoloev1900@gmail.com>
2022-12-21 16:17:38 +0100

    add CRUD create

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/comics', [ComicController::class, 'index'])->name('index');
 Route::get('/comics/{comic}', [PostController::class, 'show'])->name('show');
+Route::get('/create', [PostController::class, 'create'])->name('create');
